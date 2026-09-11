387fdb147d84b45026fb29950b87e0b5366245dc
Maanya18 <mg9690@srmist.edu.in>
2026-09-11 21:56:43 +0530

    Added Week 2 Assignment

---
+++ WEEK-2/MaskedPhoneNumberFormatter.java
@@ -0,0 +1,32 @@
+public class MaskedPhoneNumberFormatter {
+
+    public static String maskPhoneNumber(String phone) {
+
+        if (phone.length() != 10) {
+            return "Invalid phone number";
+        }
+
+        for (int i = 0; i < phone.length(); i++) {
+            if (!Character.isDigit(phone.charAt(i))) {
+                return "Invalid phone number";
+            }
+        }
+
+        String lastFour =
+                phone.substring(phone.length() - 4);
+
+        StringBuilder sb = new StringBuilder("XXXXXX");
+        sb.append("-").append(lastFour);
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                maskPhoneNumber("9876543210"));
+
+        System.out.println(
+                maskPhoneNumber("98765"));
+    }
+}
