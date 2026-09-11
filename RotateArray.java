0d5a364a5c6fe7525fc878eb0004c8af742a550d
Maanya18 <mg9690@srmist.edu.in>
2026-09-11 23:36:17 +0530

    Added Week 3 programs

---
+++ RotateArray.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public static int[] rotateArray(int[] nums, int k) {
+
+        int n = nums.length;
+
+        k = k % n;
+
+        int[] rotated = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            rotated[(i + k) % n] = nums[i];
+        }
+
+        return rotated;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+
+        System.out.println(
+                Arrays.toString(
+                        rotateArray(nums, 3)));
+    }
+}
